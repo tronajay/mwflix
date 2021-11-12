1a4c6218f37a4097af76bf6d1ed35c80c20de880
Ajay Patel <ajpatel762@gmail.com>
2021-11-12 10:41:52 +0530

    Heroku Debug

--- Moviesite/settings.py
+++ Moviesite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [' https://mwflix.herokuapp.com','localhost']
+ALLOWED_HOSTS = [' mwflix.herokuapp.com','localhost']
 
 
 # Application definition
